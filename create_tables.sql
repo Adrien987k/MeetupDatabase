ec124a4d1b8f65f440438bb1d8beb175f51d952c
Adrien987 <adrien.bardes@dbmail.com>
2018-03-23 10:34:06 +0100

    update

--- create_tables.sql
+++ create_tables.sql
@@ -14,19 +14,19 @@ CREATE TABLE Category(
 CREATE TABLE City(
        city_id INTEGER PRIMARY KEY,
        city_name VARCHAR(50) NOT NULL,
-       country VARCHAR(50) NO NULL,
+       country VARCHAR(50) NOT NULL,
        latitude REAL NOT NULL,
        longitude REAL NOT NULL,
        state VARCHAR(50));
 
-CREATE TYPE USER_STATUS_TYPE AS ENUM ('active')
+CREATE TYPE USER_STATUS_TYPE AS ENUM ('active');
 CREATE TABLE Member(
        user_id INTEGER PRIMARY KEY,
        user_name VARCHAR(50) NOT NULL,
        user_status USER_STATUS_TYPE NOT NULL,
        joined DATE NOT NULL,
        bio TEXT,
-       city_id INTEGER,
+       city_id INTEGER
        FOREIGN KEY city_id REFERENCES City(city_id)
                ON DELETE SET NULL,
        visited DATE NOT NULL,
@@ -37,27 +37,27 @@ CREATE TABLE Member(
        photo.type VARCHAR(20));
 
 CREATE TABLE Moderator_category(
-       user_id INTEGER,
+       user_id INTEGER
        FOREIGN KEY user_id REFERENCES Member(user_id)
                ON DELETE CASCADE,
-       category_id INTEGER,
+       category_id INTEGER
        FOREIGN KEY REFERENCES Category(category_id)
-               ON DELETE CASCADE,);
+               ON DELETE CASCADE);
 
 CREATE TABLE Group_organizer(
        group_id INTEGER,
-       FOREIGN KEY REFERENCES Group(group_id)
+       FOREIGN KEY group_id REFERENCES Group(group_id)
                ON DELETE CASCADE,
        user_id INTEGER,
-       FOREIGN KEY REFERENCES Member(user_id)
+       FOREIGN KEY user_id REFERENCES Member(user_id)
                ON DELETE CASCADE);
 
 CREATE TABLE Group_follower(
        group_id INTEGER,
-       FOREIGN KEY REFERENCES Group(group_id)
+       FOREIGN KEY group_id REFERENCES Group(group_id)
                ON DELETE CASCADE,
        user_id INTEGER,
-       FOREIGN KEY REFERENCES Member(user_id)
+       FOREIGN KEY user_id REFERENCES Member(user_id)
                ON DELETE CASCADE);
 
 
@@ -82,17 +82,21 @@ CREATE TABLE Venue(
        venue_id INTEGER PRIMARY KEY,
        address_1 VARCHAR(50) NOT NULL,
        venue_name VARCHAR(50) NOT NULL,
-       city_id INTEGER FOREIGN KEY REFERENCES City(city_id),
+       city_id INTEGER,
+       FOREIGN KEY city_id REFERENCES City(city_id)
+               ON DELETE CASCADE
        rating.average REAL NOT NULL,
-rating.count INTEGER NOT NULL);
+       rating.count INTEGER NOT NULL);
 
 
-CREATE TYPE EVENT_STATUS_TYPE AS ENUM('cancelled', 'upcoming', 'past', 'proposed')
-CREATE TYPE VISIBILITY_TYPE AS ENUM('public', 'member', 'public_limited')
+CREATE TYPE EVENT_STATUS_TYPE AS ENUM('cancelled', 'upcoming', 'past', 'proposed');
+CREATE TYPE VISIBILITY_TYPE AS ENUM('public', 'member', 'public_limited');
 
 CREATE TABLE Event(
        event_id INTEGER PRIMARY KEY,
-       group_id INTEGER FOREIGN KEY REFERENCES Group(group_id),
+       group_id INTEGER,
+       FOREIGN KEY group_id REFERENCES Group(group_id)
+               ON DELETE CASCADE,
        created DATE NOT NULL,
        description TEXT,
        duration INTEGER NOT NULL,
@@ -101,8 +105,12 @@ CREATE TABLE Event(
        event_name VARCHAR(50) NOT NULL,
        event_status EVENT_STATUS_TYPE NOT NULL,
        event_time DATE NOT NULL,
-       fee_id INTEGER FOREIGN KEY REFERENCES Fee(fee_id),
-       venue_id INTEGER FOREIGN KEY REFERENCES Venue(venue_id),
+       fee_id INTEGER,
+       FOREIGN KEY fee_id REFERENCES Fee(fee_id)
+               ON DELETE SET NULL,
+       venue_id INTEGER,
+       FOREIGN KEY venue_id REFERENCES Venue(venue_id)
+               ON DELETE CASCADE,
        updated DATE,
        visibility VISIBILITY_TYPE NOT NULL,
        why TEXT,
@@ -114,19 +122,27 @@ CREATE TABLE Event(
        yes_rsvp_count INTEGER NOT NULL);
 
 CREATE TABLE Event_organizer(
-       event_id INTEGER REFERENCES Event(event_id),
-       user_id INTEGER REFERENCES Member(user_id));
+       event_id INTEGER
+       FOREIGN KEY REFERENCES Event(event_id)
+                  ON DELETE CASCADE,
+       user_id INTEGER
+       FOREIGN KEY REFERENCES Member(user_id)
+                  ON DELETE CASCADE);
 
-CREATE TYPE JOIN_MODE_TYPE AS ENUM('open', 'closed', 'approval')
+CREATE TYPE JOIN_MODE_TYPE AS ENUM('open', 'closed', 'approval');
 
 CREATE TABLE Group(
        group_id INTEGER PRIMARY KEY,
        created DATE NOT NULL,
-       category_id FOREIGN KEY REFERENCES Category(category_id)
+       category_id INTEGER,
+       FOREIGN KEY category_id REFERENCES Category(category_id)
+               ON DELETE CASCADE
        description TEXT NOT NULL,
        join_mode JOIN_MODE_TYPE NOT NULL,
        group_name VARCHAR(50),
-       city_id INTEGER FOREIGN KEY REFERENCES City(city_id)
+       city_id INTEGER,
+       FOREIGN KEY city_id REFERENCES City(city_id)
+               ON DELETE SET NULL,
        visibility VISIBILITY_TYPE NOT NULL,
        who VARCHAR(50),
        photo.base_url TEXT,
