46f1cb2d510a3d23a40ce52eb3a54de0e46666ab
Adrien987 <adrien.bardes@dbmail.com>
2018-04-06 10:36:46 +0200

    Create database working

--- create_tables.sql
+++ create_tables.sql
@@ -1,15 +1,24 @@
+
 CREATE TYPE MANAGER_TYPE_TYPE AS ENUM ('Moderator', 'Administrator');
+CREATE TYPE EVENT_STATUS_TYPE AS ENUM('cancelled', 'upcoming', 'past', 'proposed');
+CREATE TYPE VISIBILITY_TYPE AS ENUM('public', 'member', 'public_limited');
+CREATE TYPE ACCEPTS_TYPE AS ENUM ('cash', 'paypal', 'wepal', 'amazon', 'others');
+CREATE TYPE USER_STATUS_TYPE AS ENUM ('active');
+CREATE TYPE JOIN_MODE_TYPE AS ENUM('open', 'closed', 'approval');
+
 
 CREATE TABLE WebsiteManager(
        user_id INTEGER PRIMARY KEY,
        user_name VARCHAR(50) NOT NULL,
        joined TIMESTAMP NOT NULL,
-       manager_type MANAGER_TYPE_TYPE NOT NULL);
+       manager_type MANAGER_TYPE_TYPE NOT NULL
+);
 
 CREATE TABLE Category(
        category_id INTEGER PRIMARY KEY,
        category_name VARCHAR(50) NOT NULL,
-       shortname VARCHAR(20) NOT NULL);
+       shortname VARCHAR(20) NOT NULL
+);
 
 CREATE TABLE City(
        city_id INTEGER PRIMARY KEY,
@@ -17,35 +26,42 @@ CREATE TABLE City(
        country VARCHAR(50) NOT NULL,
        latitude REAL NOT NULL,
        longitude REAL NOT NULL,
-       state VARCHAR(50));
+       state VARCHAR(50)
+);
 
-CREATE TYPE USER_STATUS_TYPE AS ENUM ('active');
+CREATE TABLE Groups(
+       group_id INTEGER PRIMARY KEY,
+       created TIMESTAMP NOT NULL,
+       category_id INTEGER REFERENCES Category(category_id) ON DELETE CASCADE,
+       description TEXT NOT NULL,
+       join_mode JOIN_MODE_TYPE NOT NULL,
+       organizer INTEGER,
+       group_name VARCHAR(50),
+       city_id INTEGER REFERENCES City(city_id) ON DELETE SET NULL,
+       visibility VISIBILITY_TYPE NOT NULL,
+       who VARCHAR(50),
+       photo_photo_link VARCHAR(200)
+);
+
 CREATE TABLE Member(
        member_id INTEGER PRIMARY KEY,
        member_name VARCHAR(50) NOT NULL,
        member_status USER_STATUS_TYPE NOT NULL,
        joined TIMESTAMP NOT NULL,
        bio TEXT,
-       city_id INTEGER
-       FOREIGN KEY city_id REFERENCES City(city_id)
-               ON DELETE SET NULL,
-       visited TIMESTAMP NOT NULL);
+       city_id INTEGER REFERENCES City(city_id) ON DELETE SET NULL,
+       visited TIMESTAMP NOT NULL
+);
 
 CREATE TABLE Moderator_category(
-       user_id INTEGER
-       FOREIGN KEY user_id REFERENCES Member(user_id)
-               ON DELETE CASCADE,
-       category_id INTEGER,
-       FOREIGN KEY category_id REFERENCES Category(category_id)
-               ON DELETE CASCADE);
+       user_id INTEGER REFERENCES Member(member_id) ON DELETE CASCADE,
+       category_id INTEGER REFERENCES Category(category_id) ON DELETE CASCADE
+);
 
 CREATE TABLE Group_follower(
-       group_id INTEGER,
-       FOREIGN KEY group_id REFERENCES Group(group_id)
-               ON DELETE CASCADE,
-       user_id INTEGER,
-       FOREIGN KEY user_id REFERENCES Member(user_id)
-               ON DELETE CASCADE);
+       group_id INTEGER REFERENCES Groups(group_id) ON DELETE CASCADE,
+       user_id INTEGER REFERENCES Member(member_id) ON DELETE CASCADE
+);
 
 
 CREATE TABLE Topic(
@@ -53,9 +69,8 @@ CREATE TABLE Topic(
        description TEXT,
        urlkey VARCHAR(20) NOT NULL,
        topic_name VARCHAR(50) NOT NULL,
-       parent_topic_id INTEGER);
-
-CREATE TYPE ACCEPTS_TYPE AS ENUM ('cash', 'paypal', 'wepal', 'amazon', 'others');
+       parent_topic_id INTEGER
+);
 
 CREATE TABLE Fee(
        fee_id INTEGER PRIMARY KEY,
@@ -63,27 +78,20 @@ CREATE TABLE Fee(
        amount REAL,
        currency VARCHAR(20),
        description TEXT,
-       required BOOLEAN NOT NULL);
+       required BOOLEAN NOT NULL
+);
 
 CREATE TABLE Venue(
        venue_id INTEGER PRIMARY KEY,
        address_1 VARCHAR(50) NOT NULL,
        venue_name VARCHAR(50) NOT NULL,
-       city_id INTEGER,
-       FOREIGN KEY city_id REFERENCES City(city_id)
-               ON DELETE CASCADE
-       rating.average REAL NOT NULL,
-       rating.count INTEGER NOT NULL);
-
-
-CREATE TYPE EVENT_STATUS_TYPE AS ENUM('cancelled', 'upcoming', 'past', 'proposed');
-CREATE TYPE VISIBILITY_TYPE AS ENUM('public', 'member', 'public_limited');
+       city_id INTEGER REFERENCES City(city_id) ON DELETE CASCADE,
+       rating_average REAL NOT NULL,
+       rating_count INTEGER NOT NULL);
 
 CREATE TABLE Event(
        event_id INTEGER PRIMARY KEY,
-       group_id INTEGER,
-       FOREIGN KEY group_id REFERENCES Group(group_id)
-               ON DELETE CASCADE,
+       group_id INTEGER REFERENCES Groups(group_id) ON DELETE CASCADE,
        created TIMESTAMP NOT NULL,
        description TEXT,
        duration INTEGER NOT NULL,
@@ -92,45 +100,20 @@ CREATE TABLE Event(
        event_name VARCHAR(50) NOT NULL,
        event_status EVENT_STATUS_TYPE NOT NULL,
        event_time TIMESTAMP NOT NULL,
-       fee_id INTEGER,
-       FOREIGN KEY fee_id REFERENCES Fee(fee_id)
-               ON DELETE SET NULL,
-       venue_id INTEGER,
-       FOREIGN KEY venue_id REFERENCES Venue(venue_id)
-               ON DELETE CASCADE,
+       fee_id INTEGER REFERENCES Fee(fee_id) ON DELETE SET NULL,
+       venue_id INTEGER REFERENCES Venue(venue_id) ON DELETE CASCADE,
        updated TIMESTAMP,
        visibility VISIBILITY_TYPE NOT NULL,
        why TEXT,
        headcount INTEGER,
        rating_average REAL,
-       raiting.count INTEGER NOT NULL,
+       raiting_count INTEGER NOT NULL,
        rsvp_limit INTEGER NOT NULL,
        maybe_rsvp_count INTEGER NOT NULL,
-       yes_rsvp_count INTEGER NOT NULL);
+       yes_rsvp_count INTEGER NOT NULL
+);
 
 CREATE TABLE Event_organizer(
-       event_id INTEGER,
-       FOREIGN KEY event_id REFERENCES Event(event_id)
-                  ON DELETE CASCADE,
-       user_id INTEGER,
-       FOREIGN KEY user_id  REFERENCES Member(user_id)
-                  ON DELETE CASCADE);
-
-CREATE TYPE JOIN_MODE_TYPE AS ENUM('open', 'closed', 'approval');
-
-CREATE TABLE Group(
-       group_id INTEGER PRIMARY KEY,
-       created TIMESTAMP NOT NULL,
-       category_id INTEGER,
-       FOREIGN KEY category_id REFERENCES Category(category_id)
-               ON DELETE CASCADE
-       description TEXT NOT NULL,
-       join_mode JOIN_MODE_TYPE NOT NULL,
-       organizer INTEGER,
-       group_name VARCHAR(50),
-       city_id INTEGER,
-       FOREIGN KEY city_id REFERENCES City(city_id)
-               ON DELETE SET NULL,
-       visibility VISIBILITY_TYPE NOT NULL,
-       who VARCHAR(50),
-       photo.photo_link VARCHAR(200));
+       event_id INTEGER REFERENCES Event(event_id) ON DELETE CASCADE,
+       user_id INTEGER REFERENCES Member(member_id) ON DELETE CASCADE
+);
