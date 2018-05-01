523db05872d127040aef7dc061767848333e11a7
valrictin <valentin.richard@live.fr>
2018-05-01 10:28:01 +0200

    bonne_version_create_tables

--- create_tables.sql
+++ create_tables.sql
@@ -1,9 +1,9 @@
 
 CREATE TYPE MANAGER_TYPE_TYPE AS ENUM ('Moderator', 'Administrator');
 CREATE TYPE EVENT_STATUS_TYPE AS ENUM('cancelled', 'upcoming', 'past', 'proposed');
-CREATE TYPE VISIBILITY_TYPE AS ENUM('public', 'member', 'public_limited');
-CREATE TYPE ACCEPTS_TYPE AS ENUM ('cash', 'paypal', 'wepal', 'amazon', 'others');
-CREATE TYPE USER_STATUS_TYPE AS ENUM ('active');
+CREATE TYPE VISIBILITY_TYPE AS ENUM('public', 'member', 'public_limited', 'members');
+CREATE TYPE ACCEPTS_TYPE AS ENUM ('cash', 'paypal', 'wepal', 'amazon', 'others', 'wepay');
+CREATE TYPE USER_STATUS_TYPE AS ENUM ('active','prereg');
 CREATE TYPE JOIN_MODE_TYPE AS ENUM('open', 'closed', 'approval');
 
 
@@ -20,8 +20,13 @@ CREATE TABLE Category(
        shortname VARCHAR(20) NOT NULL
 );
 
+CREATE TABLE Moderator_category(
+       user_id INTEGER REFERENCES Member(member_id) ON DELETE CASCADE,
+       category_id INTEGER REFERENCES Category(category_id) ON DELETE CASCADE
+);
+
 CREATE TABLE City(
-       city_id INTEGER PRIMARY KEY,
+       city_id INTEGER NOT NULL,
        city_name VARCHAR(50) PRIMARY KEY,
        country VARCHAR(50) NOT NULL,
        latitude REAL NOT NULL,
@@ -35,60 +40,57 @@ CREATE TABLE Groups(
        category_id INTEGER REFERENCES Category(category_id) ON DELETE CASCADE,
        description TEXT NOT NULL,
        join_mode JOIN_MODE_TYPE NOT NULL,
-       organizer INTEGER,
-       group_name VARCHAR(50),
-       city_id INTEGER REFERENCES City(city_id) ON DELETE SET NULL,
+       organizer_id INTEGER,
+       group_name VARCHAR(100),
+       city_name VARCHAR(50) REFERENCES City(city_name) ON DELETE SET NULL,
        visibility VISIBILITY_TYPE NOT NULL,
        who VARCHAR(50),
        photo_photo_link VARCHAR(200)
 );
 
 CREATE TABLE Member(
-       member_id INTEGER PRIMARY KEY,
+       member_id INTEGER,
        member_name VARCHAR(50) NOT NULL,
        member_status USER_STATUS_TYPE NOT NULL,
        joined TIMESTAMP NOT NULL,
        bio TEXT,
-       city_name VARCHAR(50) REFERENCES City(city_name) ON DELETE SET NULL,
-       visited TIMESTAMP NOT NULL
+       city_name VARCHAR(50), -- REFERENCES City(city_name) ON DELETE SET NULL,
+       visited TIMESTAMP NOT NULL,
+       group_id INTEGER,
+       PRIMARY KEY (member_id,group_id)
 );
-
-CREATE TABLE Moderator_category(
-       user_id INTEGER REFERENCES Member(member_id) ON DELETE CASCADE,
-       category_id INTEGER REFERENCES Category(category_id) ON DELETE CASCADE
-);
-
-CREATE TABLE Group_followed(
-       group_id INTEGER REFERENCES Groups(group_id) ON DELETE CASCADE,
-       user_id INTEGER REFERENCES Member(member_id) ON DELETE CASCADE
-);
-
 
 CREATE TABLE Topic(
        topic_id INTEGER PRIMARY KEY,
        description TEXT,
-       urlkey VARCHAR(20) NOT NULL,
+       urlkey VARCHAR(50) NOT NULL,
        topic_name VARCHAR(50) NOT NULL,
        parent_topic_id INTEGER
 );
 
+CREATE TABLE Group_topic(
+       group_id INTEGER REFERENCES Groups(group_id) ON DELETE CASCADE,
+       topic_id INTEGER REFERENCES Topic(topic_id) ON DELETE CASCADE
+);
+
 CREATE TABLE Venue(
        venue_id INTEGER PRIMARY KEY,
-       address_1 VARCHAR(50) NOT NULL,
-       venue_name VARCHAR(50) NOT NULL,
-       city_name VARCHAR(50) REFERENCES City(city_name) ON DELETE CASCADE
+       address_1 VARCHAR(200),
+       venue_name VARCHAR(200),
+       city_name VARCHAR(50), -- REFERENCES City(city_name) ON DELETE CASCADE,
        rating_average REAL NOT NULL,
-       rating_count INTEGER NOT NULL);
+       rating_count REAL NOT NULL
+);
 
 CREATE TABLE Event(
-       event_id INTEGER PRIMARY KEY,
+       event_id VARCHAR(20) PRIMARY KEY,
        group_id INTEGER REFERENCES Groups(group_id) ON DELETE CASCADE,
        created TIMESTAMP NOT NULL,
        description TEXT,
        duration INTEGER NOT NULL,
        photo_url TEXT,
        how_to_find_us TEXT,
-       event_name VARCHAR(50) NOT NULL,
+       event_name VARCHAR(100) NOT NULL,
        event_status EVENT_STATUS_TYPE NOT NULL,
        event_time TIMESTAMP NOT NULL,
        venue_id INTEGER REFERENCES Venue(venue_id) ON DELETE CASCADE,
@@ -97,15 +99,18 @@ CREATE TABLE Event(
        why TEXT,
        headcount INTEGER,
        rating_average REAL,
-       raiting_count INTEGER NOT NULL,
+       rating_count INTEGER NOT NULL,
        rsvp_limit INTEGER NOT NULL,
        maybe_rsvp_count INTEGER NOT NULL,
        yes_rsvp_count INTEGER NOT NULL,
-       fee_accpets ACCEPTS_TYPE NOT NULL,
+       waitlist_count INTEGER NOT NULL,
+       fee_accepts ACCEPTS_TYPE NOT NULL,
        fee_amount REAL,
        fee_required BOOLEAN NOT NULL,
-       repinned BOOLEAN);
+       repinned BOOLEAN
+);
 
 CREATE TABLE Topic_followed(
        topic_id INTEGER REFERENCES Topic(topic_id) ON DELETE CASCADE,
-       member_id INTEGER REFERENCES Member(memebr_id) ON DELETE CASCADE);
+       member_id INTEGER REFERENCES Member(member_id) ON DELETE CASCADE
+);
