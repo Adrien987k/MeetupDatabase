77433b73556c39e8a4f191c494ce2ea4b7a87463
ADRIEN BARDES <adrien.bardes@etu.u-pec.fr>
2018-05-04 10:40:38 +0200

    models.py

--- meetup/meetup/settings.py
+++ meetup/meetup/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'meetup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'meetup',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
