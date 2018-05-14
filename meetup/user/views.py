f750d48274f02f757f7aa5aee7cb134273eaf041
valrictin <valentin.richard@live.fr>
2018-05-14 23:40:00 +0200

    14-05,23:39

--- meetup/user/views.py
+++ meetup/user/views.py
@@ -25,11 +25,9 @@ def Auth(request):
     return render(request, 'user/index.html', {'form': form})
 
 
-class UserIndex(generic.ListView):
-    template_name = 'user/user_index.html'
-
-    def get_queryset(self):
-        return()
+def UserIndex(request):
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return render(request, 'user/user_index.html', {'now' : now})
 
 # *** Category ***
 
@@ -62,8 +60,11 @@ def CityDetail(request, city_name):
     city = get_object_or_404(City, pk=city_name)
     member_count = Member.objects.filter(city_name = city_name).distinct('member_id').count()
     group_count = Groups.objects.filter(city_name = city_name).count()
-    venue_count = Venue.objects.filter(city_name = city_name).count()
-    return render(request, 'user/city_detail.html', {'city': city, 'member_count' : member_count, 'group_count' : group_count, 'venue_count' : venue_count})
+    venues = Venue.objects.filter(city_name = city_name)
+    venue_count = venues.count
+    event_count = Events.objects.filter(venue_id__in=[q.venue_id for q in venues]).distinct('event_name').count()
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return render(request, 'user/city_detail.html', {'city': city, 'member_count' : member_count, 'group_count' : group_count, 'venue_count' : venue_count, 'event_count' : event_count, 'now' : now})
 
 # *** Topics ***
 
@@ -84,12 +85,15 @@ def TopicDetail(request, topic_id):
     except Topic.DoesNotExist:
         parent_topic = Topic.objects.none()
         have_parent = False
+    now = datetime.datetime.now()
+    prev = (now-datetime.timedelta(days=365)).strftime("%Y-%m-%d %H:%M:%S")
+    now = now.strftime("%Y-%m-%d %H:%M:%S")
 
     member_query = TopicFollowed.objects.filter(topic_id=topic_id)
     member_id_list = [q.member_id for q in member_query]
     member_count = Member.objects.filter(member_id__in=member_id_list).distinct('member_id').count()
     group_count = GroupTopic.objects.filter(topic_id = topic_id).count
-    return render(request, 'user/topic_detail.html', {'topic': topic, 'parent_topic' : parent_topic, 'group_count' : group_count, 'have_parent' : have_parent, 'member_count' : member_count})
+    return render(request, 'user/topic_detail.html', {'topic': topic, 'parent_topic' : parent_topic, 'group_count' : group_count, 'have_parent' : have_parent, 'member_count' : member_count, 'now' : now, 'prev' : prev})
 
 # *** Groups ***
 
@@ -122,12 +126,14 @@ def GroupDetail(request, group_id):
         organizer = organizers[0]
         have_org = True
 
-    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    now = datetime.datetime.now()
+    prev = (now-datetime.timedelta(days=365)).strftime("%Y-%m-%d %H:%M:%S")
+    now = now.strftime("%Y-%m-%d %H:%M:%S")
     member_count = Member.objects.filter(group_id=group_id).distinct('member_id').count()
     topic_query = GroupTopic.objects.filter(group_id = group_id)
     topic_list = [q.topic_id for q in topic_query]
     topics = Topic.objects.filter(topic_id__in=topic_list)
-    return render(request, 'user/group_detail.html', {'group': group, 'topics' : topics, 'organizer' : organizer,'have_org' : have_org, 'member_count' : member_count, 'now' : now})
+    return render(request, 'user/group_detail.html', {'group': group, 'topics' : topics, 'organizer' : organizer,'have_org' : have_org, 'member_count' : member_count, 'now' : now, 'prev' : prev})
 
 # *** Venue ***
 
@@ -155,7 +161,34 @@ def VenueIndex(request, filt, filt_pk, page):
 def VenueDetail(request, venue_id):
     venue = get_object_or_404(Venue, pk=venue_id)
     rating_count = int(venue.rating_count)
-    return render(request, 'user/venue_detail.html', {'venue': venue, 'rating_count': rating_count})
+    if rating_count == -1:
+        rating_count = 0
+    event_count = Events.objects.filter(venue_id=venue_id).distinct('event_name').count()
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    votes = range(0,6)
+    return render(request, 'user/venue_detail.html', {'venue': venue, 'rating_count': rating_count, 'event_count' : event_count, 'now' : now, 'votes' : votes})
+
+def VenueVote(request, venue_id):
+    venue = get_object_or_404(Venue, pk=venue_id)
+    rating_count = int(venue.rating_count)
+    if rating_count == -1:
+        rating_count = 0
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    votes = range(0,6)
+    event_count = Events.objects.filter(venue_id=venue_id).distinct('event_name').count()
+    try:
+        vote = int(request.POST['choice'])
+    except KeyError:
+        # Redisplay the question voting form.
+        return render(request, 'user/venue_detail.html', {'venue': venue, 'rating_count': rating_count, 'event_count' : event_count, 'now' : now, 'error_message' : "You didn't select a choice", 'votes' : votes})
+    else:
+        venue.rating_average = (venue.rating_average * rating_count + vote) / (rating_count + 1)
+        venue.rating_count = rating_count + 1
+        venue.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return render(request, 'user/venue_results.html', {'venue' : venue})
 
 # *** Member ***
 
@@ -215,23 +248,30 @@ def EventIndex(request,filt,filt_pk,date):
         event_list = Events.objects.filter(group_id=filt_pk) 
         filt_name = Groups.objects.get(pk=filt_pk).group_name
     elif filt == "city":
-        event_list = Events.objects.filter(city_name=filt_pk)
+        venue_query = Venue.objects.filter(city_name=filt_pk)
+        event_list = Events.objects.filter(venue_id__in=[q.venue_id for q in venue_query])
         filt_name = filt_pk
     elif filt == "topic":
         group_query = GroupTopic.objects.filter(topic_id=filt_pk)
         group_id_list = [q.group_id for q in group_query]
         event_list = Events.objects.filter(group_id__in=group_id_list)
         filt_name = Topic.objects.get(pk=filt_pk).topic_name
+    elif filt == "venue":
+        event_list = Events.objects.filter(venue_id = filt_pk)
+        filt_name = Venue.objects.get(pk=filt_pk).venue_name
     else:
         event_list = Events.objects.all()
         filt_name = ""
 
-    event_list = [q for q in event_list if q.event_time >= date and q.event_time < next_date]
+    event_list = [q for q in event_list.distinct('event_name') if q.event_time >= date and q.event_time < next_date]
     return render(request, 'user/event_index.html', {'event_list': event_list, 'filt' : filt, 'filt_name' : filt_name, 'filt_pk' : filt_pk, 'date' : date.strftime("%Y-%m-%d %H:%M:%S"), 'next_date' : next_date.strftime("%Y-%m-%d %H:%M:%S"), 'prev_date' : prev_date.strftime("%Y-%m-%d %H:%M:%S")})
                                     
 
 def EventDetail(request, event_id):
     event = get_object_or_404(Events, pk=event_id)
+    rating_count = event.rating_count
+    if rating_count == -1:
+        rating_count = 0
     duration = event.duration / 3600
     try:
         org_group = Groups.objects.get(pk=event.group_id)
@@ -243,5 +283,40 @@ def EventDetail(request, event_id):
         have_venue = True
     except Venue.DoesNotExist:
         have_venue = False
+    past = datetime.datetime.now().replace(tzinfo=utc) > event.event_time
+    votes = range(0,6)
    
-    return render(request, 'user/event_detail.html', {'event': event, 'duration' : duration, 'org_group' : org_group, 'have_org' : have_org, 'venue' : venue, 'have_venue' : have_venue})
+    return render(request, 'user/event_detail.html', {'event': event, 'duration' : duration, 'org_group' : org_group, 'have_org' : have_org, 'venue' : venue, 'have_venue' : have_venue, 'past' : past, 'votes' : votes, 'rating_count' : rating_count})
+
+def EventVote(request, event_id):
+    event = get_object_or_404(Events, pk=event_id)
+    rating_count = event.rating_count
+    if rating_count == -1:
+        rating_count = 0
+    duration = event.duration / 3600
+    try:
+        org_group = Groups.objects.get(pk=event.group_id)
+        have_org = True
+    except Groups.DoesNotExist:
+        have_org = False
+    try:
+        venue = Venue.objects.get(pk=event.venue_id)
+        have_venue = True
+    except Venue.DoesNotExist:
+        have_venue = False
+    past = datetime.datetime.now().replace(tzinfo=utc) > event.event_time
+    votes = range(0,6)
+    
+    try:
+        vote = int(request.POST['choice'])
+    except KeyError:
+        # Redisplay the question voting form.
+        return render(request, 'user/event_detail.html', {'event': event, 'duration' : duration, 'org_group' : org_group, 'have_org' : have_org, 'venue' : venue, 'have_venue' : have_venue, 'past' : past, 'rating_count' : rating_count, 'error_message' : "You didn't select a choice", 'votes' : votes})
+    else:
+        event.rating_average = (event.rating_average * rating_count + vote) / (rating_count + 1)
+        event.rating_count = rating_count + 1
+        event.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return render(request, 'user/event_results.html', {'event' : event})
